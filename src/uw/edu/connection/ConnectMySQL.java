36327f883db5626fc469866c70f5216884f6e768
Karthik Tunga Gopinath <karthik@Gideon.local>
2019-03-14 18:48:25 -0700

    adding patient update

--- src/uw/edu/connection/ConnectMySQL.java
+++ src/uw/edu/connection/ConnectMySQL.java
@@ -87,59 +87,98 @@ public class ConnectMySQL {
 	}
 	
 	//get all users from database - patient table
-		public PatientVO  getPatientDetails(String patientId)
-				throws Exception {
-			//System.out.println("Inside get patient details");
-			PatientVO patient = new PatientVO();
-			try {
-				Connection c = getConnection();
-				
-				PreparedStatement ps = c
-						.prepareStatement("SELECT * FROM patients where id= ?");
-				ps.setString(1,patientId);
-				//System.out.println("stmt is"+ps);
-				ResultSet rs = ps.executeQuery();
-				while(rs.next()){
-				//System.out.println("name is"+rs.getString("name"));
-				patient.setName(rs.getString("name"));
-				patient.setAge(rs.getString("age"));	
-				patient.setInsurance(rs.getString("insurance"));	
-				patient.setAddress(rs.getString("address"));
-				patient.setId(rs.getString("id"));
-				}
-				closeconnection();
-				return patient;
-				
-			} catch (Exception e) {
-				throw e;
+	public PatientVO  getPatientDetails(String patientId)
+			throws Exception {
+		//System.out.println("Inside get patient details");
+		PatientVO patient = new PatientVO();
+		try {
+			Connection c = getConnection();
+			
+			PreparedStatement ps = c
+					.prepareStatement("SELECT * FROM patients where id= ?");
+			ps.setString(1,patientId);
+			//System.out.println("stmt is"+ps);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+			//System.out.println("name is"+rs.getString("name"));
+			patient.setName(rs.getString("name"));
+			patient.setAge(rs.getString("age"));	
+			patient.setInsurance(rs.getString("insurance"));	
+			patient.setAddress(rs.getString("address"));
+			patient.setId(rs.getString("id"));
 			}
+			closeconnection();
+			return patient;
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	//add new  patient into database - patient table
+	public void  addNewPatient(PatientVO patient)
+			throws Exception {
+		//System.out.println("Inside get patient details");
+		//System.out.println(" database name is:"+patient.getName());
+		try {
+			Connection c = getConnection();
+			
+			String query = "insert into patients (id, name, age, insurance, address)"
+			        + " values (?, ?, ?, ?, ?)";
+			
+			PreparedStatement ps = c
+					.prepareStatement(query);
+			ps.setString(1,patient.getId());
+			ps.setString(2,patient.getName());
+			ps.setString(3,patient.getAge());
+			ps.setString(4,patient.getInsurance());
+			ps.setString(5,patient.getAddress());
+			//System.out.println("stmt is"+ps);
+			ps.execute();
+			closeconnection();
+			
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	public void updatePatientInfo(PatientVO patient) 
+		throws Exception {
+		
+		PatientVO patientToUpdate = getPatientDetails(patient.getId());
+		
+		if(patient.getAge() != null && !patient.getAge().isEmpty()) {
+			patientToUpdate.setAge(patient.getAge());
 		}
 		
-		//add new  patient into database - patient table
-				public void  addNewPatient(PatientVO patient)
-						throws Exception {
-					//System.out.println("Inside get patient details");
-					//System.out.println(" database name is:"+patient.getName());
-					try {
-						Connection c = getConnection();
-						
-						String query = "insert into patients (id, name, age, insurance, address)"
-						        + " values (?, ?, ?, ?, ?)";
-						
-						PreparedStatement ps = c
-								.prepareStatement(query);
-						ps.setString(1,patient.getId());
-						ps.setString(2,patient.getName());
-						ps.setString(3,patient.getAge());
-						ps.setString(4,patient.getInsurance());
-						ps.setString(5,patient.getAddress());
-						//System.out.println("stmt is"+ps);
-						ps.execute();
-						closeconnection();
-						
-					} catch (Exception e) {
-						throw e;
-					}
-				}
-
+		String newAddress = patient.getAddress();
+		if(newAddress != null && !newAddress.isEmpty()) {
+			patientToUpdate.setAddress(newAddress);
+		}
+		
+		String newInsurance = patient.getInsurance();
+		if(newInsurance != null && !newInsurance.isEmpty()) {
+			patientToUpdate.setInsurance(newInsurance);
+		}
+		
+		// TODO(Siri) should we allow update of name ??
+		String newName = patient.getName();
+		if(newName != null && !newName.isEmpty()) {
+			patientToUpdate.setName(newName);
+		}
+		
+		Connection c = getConnection();
+		
+		String query = String.format("UPDATE patients SET name = '%s', age = '%s',  insurance ='%s', address = '%s' WHERE id = '%s'",
+				patientToUpdate.getName(),
+				patientToUpdate.getAge(),
+				patientToUpdate.getInsurance(),
+				patientToUpdate.getAddress(),
+				patientToUpdate.getId());
+				
+		PreparedStatement ps = c
+				.prepareStatement(query);
+		ps.execute();
+		closeconnection();
+	}
 }
--- src/uw/edu/webservice/GetPatientDetailService.java
+++ src/uw/edu/webservice/GetPatientDetailService.java
@@ -7,12 +7,15 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 import uw.edu.VO.PatientVO;
 import uw.edu.controller.PatientDetailController;
@@ -63,6 +66,35 @@ public class GetPatientDetailService {
 
 	}
 	
+	@PUT
+	@Path("/updatepatientdetails")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String update(@FormParam("id") String id,
+			@FormParam("name") String name,
+			@FormParam("age") String age,
+			@FormParam("address") String address,
+			@FormParam("insurance") String insurance)  {
+        System.out.println("Inside update method -> "+id + ".");
+        if(id == null || id.isEmpty()) {
+        	return "Failed! Input is null";
+        }
+		PatientVO patient = new PatientVO();
+		patient.setId(id);
+		patient.setAddress(address);
+		patient.setAge(age);
+		patient.setInsurance(insurance);
+		patient.setName(name);
+		System.out.println(String.format("Id=%s, name=%s, age=%s, address=%s", id, name, age,address));
+		PatientDetailController pc = new PatientDetailController();
+		try {
+			pc.updatePatient(patient);
+			return "Patient info successfully update";
+		} catch(Exception ex) {
+			System.out.println(ex);
+			return "Failed to update patient info";
+		}
+	}
+	
 	public String addNewPatient(PatientVO patient){
 		
 		try {
@@ -74,10 +106,6 @@ public class GetPatientDetailService {
 			//System.out.println(e.getMessage());
 			System.out.println("Patient details are not added.Please try again");
 		}
-		return "Patient Details are added Successfully";
-		
+		return "Patient Details are added Successfully";	
 	}
-	
-
-
 }
